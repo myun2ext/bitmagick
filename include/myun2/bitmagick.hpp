f866e0c5747b920829d1dd0687baf675749db867
myun2 <myun2@nwhite.info>
2013-10-24 12:43:55 +0900

    namespace

--- include/myun2/bitmagick.hpp
+++ include/myun2/bitmagick.hpp
@@ -1,4 +1,11 @@
 #ifndef __github_com_myun2__bitmagick_HPP__
 #define __github_com_myun2__bitmagick_HPP__
 
+namespace myun2
+{
+	namespace bitmagick
+	{
+	}
+}
+
 #endif//__github_com_myun2__bitmagick_HPP__
