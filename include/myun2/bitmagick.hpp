1d12651573ca16e224a53e807d7d58d5cc3b7aaa
myun2 <myun2@nwhite.info>
2013-10-24 12:45:15 +0900

    zero.hpp Add

--- include/myun2/bitmagick.hpp
+++ include/myun2/bitmagick.hpp
@@ -1,11 +1,6 @@
 #ifndef __github_com_myun2__bitmagick_HPP__
 #define __github_com_myun2__bitmagick_HPP__
 
-namespace myun2
-{
-	namespace bitmagick
-	{
-	}
-}
+#include "myun2/bitmagick/zero.hpp"
 
 #endif//__github_com_myun2__bitmagick_HPP__
---
+++ include/myun2/bitmagick/zero.hpp
@@ -0,0 +1,13 @@
+#ifndef __github_com_myun2__bitmagick__zero_HPP__
+#define __github_com_myun2__bitmagick__zero_HPP__
+
+namespace myun2
+{
+	namespace bitmagick
+	{
+		template <typename T>
+		T zero() { return T(0); }
+	}
+}
+
+#endif//__github_com_myun2__bitmagick__zero_HPP__
