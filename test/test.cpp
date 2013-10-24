eb157c379c296e09a1539c243e70304b376af5e1
myun2 <myun2@nwhite.info>
2013-10-24 16:12:16 +0900

    fill test

--- test/test.cpp
+++ test/test.cpp
@@ -6,9 +6,9 @@ using namespace myun2::bitmagick;
 int main()
 {
 	printf("%08x\n", zero<int>());
-	printf("%08x\n", reverse<unsigned char>(0));
-	printf("%08x\n", reverse<unsigned short>(0));
-	printf("%08x\n", reverse<unsigned int>(0));
-	printf("%08x\n", reverse<unsigned long>(0));
+	printf("%08x\n", filled<unsigned char>());
+	printf("%08x\n", filled<unsigned short>());
+	printf("%08x\n", filled<unsigned int>());
+	printf("%08x\n", filled<unsigned long>());
 	return 0;
 }
